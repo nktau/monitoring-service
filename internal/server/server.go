66cd8e4e415bfd765bb135c47b29b162281368a0
n.naumov <nnaumov@ourgapps.com>
2023-06-05 15:06:01 +0300

    fix tests

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -11,7 +12,7 @@ import (
 var storage = NewMemStorage()
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-
+	// http.Error or not?
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -21,40 +22,39 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 		return
 	}
-	// Нужна ли валидация на Content-Type == text/plain?
+	w.Header().Set("Content-Type", "text/plain")
 	pathSlice := strings.Split(u.Path, "/")
-	switch len(pathSlice) {
-	case 0:
-		http.Error(w, "wrong metric type", http.StatusBadRequest)
-		return
-	case 1:
-		http.Error(w, "wrong metric name", http.StatusNotFound)
-		return
-	case 2:
-		http.Error(w, "wrong metric value", http.StatusBadRequest)
-		return
-	}
+	fmt.Println(pathSlice)
+	fmt.Println(len(pathSlice))
+	fmt.Println(pathSlice[0])
 	if pathSlice[0] != "gauge" && pathSlice[0] != "counter" {
 		http.Error(w, "wrong metric type", http.StatusBadRequest)
 		return
 	}
-	if pathSlice[1] == "gauge" || pathSlice[1] == "counter" || pathSlice[1] == "" {
+
+	if len(pathSlice) < 2 || pathSlice[1] == "gauge" || pathSlice[1] == "counter" || pathSlice[1] == "" {
 		http.Error(w, "wrong metric name", http.StatusNotFound)
 		return
 	}
-	if pathSlice[0] == "gauge" {
-		if value, err := strconv.ParseFloat(pathSlice[2], 64); err != nil {
-			http.Error(w, "wrong metric value", http.StatusBadRequest)
-			return
-		} else {
-			storage.ModifyGauge(pathSlice[1], value)
-		}
-	} else if pathSlice[0] == "counter" {
-		if value, err := strconv.ParseInt(pathSlice[2], 10, 64); err != nil {
-			http.Error(w, "wrong metric value", http.StatusBadRequest)
-			return
-		} else {
-			storage.ModifyCounter(pathSlice[1], value)
+
+	if len(pathSlice) < 3 {
+		http.Error(w, "wrong metric value", http.StatusBadRequest)
+		return
+	} else {
+		if pathSlice[0] == "gauge" {
+			if value, err := strconv.ParseFloat(pathSlice[2], 64); err != nil {
+				http.Error(w, "wrong metric value", http.StatusBadRequest)
+				return
+			} else {
+				storage.ModifyGauge(pathSlice[1], value)
+			}
+		} else if pathSlice[0] == "counter" {
+			if value, err := strconv.ParseInt(pathSlice[2], 10, 64); err != nil {
+				http.Error(w, "wrong metric value", http.StatusBadRequest)
+				return
+			} else {
+				storage.ModifyCounter(pathSlice[1], value)
+			}
 		}
 	}
 }
