26a34d4f682b28a1176f012eebecc41fd44434ce
n.naumov <nnaumov@ourgapps.com>
2023-06-05 15:47:35 +0300

    refactor server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -23,38 +22,36 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/plain")
-	pathSlice := strings.Split(u.Path, "/")
-	fmt.Println(pathSlice)
-	fmt.Println(len(pathSlice))
-	fmt.Println(pathSlice[0])
-	if pathSlice[0] != "gauge" && pathSlice[0] != "counter" {
+	pathSlice := make([]string, 3)
+	pathSlice = strings.Split(u.Path, "/")
+
+	if len(pathSlice) == 1 && pathSlice[0] != "gauge" && pathSlice[0] != "counter" {
 		http.Error(w, "wrong metric type", http.StatusBadRequest)
 		return
 	}
-
-	if len(pathSlice) < 2 || pathSlice[1] == "gauge" || pathSlice[1] == "counter" || pathSlice[1] == "" {
+	if len(pathSlice) == 2 && pathSlice[1] == "" || len(pathSlice) == 1 {
 		http.Error(w, "wrong metric name", http.StatusNotFound)
 		return
 	}
-
 	if len(pathSlice) < 3 {
 		http.Error(w, "wrong metric value", http.StatusBadRequest)
 		return
-	} else {
-		if pathSlice[0] == "gauge" {
-			if value, err := strconv.ParseFloat(pathSlice[2], 64); err != nil {
-				http.Error(w, "wrong metric value", http.StatusBadRequest)
-				return
-			} else {
-				storage.ModifyGauge(pathSlice[1], value)
-			}
-		} else if pathSlice[0] == "counter" {
-			if value, err := strconv.ParseInt(pathSlice[2], 10, 64); err != nil {
-				http.Error(w, "wrong metric value", http.StatusBadRequest)
-				return
-			} else {
-				storage.ModifyCounter(pathSlice[1], value)
-			}
+	}
+	if pathSlice[0] == "gauge" {
+		value, err := strconv.ParseFloat(pathSlice[2], 64)
+		if err != nil {
+			http.Error(w, "wrong metric value", http.StatusBadRequest)
+			return
+		}
+		storage.ModifyGauge(pathSlice[1], value)
+	}
+	if pathSlice[0] == "counter" {
+		value, err := strconv.ParseInt(pathSlice[2], 10, 64)
+		if err != nil {
+			http.Error(w, "wrong metric value", http.StatusBadRequest)
+			return
+		} else {
+			storage.ModifyCounter(pathSlice[1], value)
 		}
 	}
 }
