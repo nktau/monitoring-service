b0b8a4bacd5f1f9827446f6b0188bab992e4f34f
n.naumov <nnaumov@ourgapps.com>
2023-06-07 11:06:10 +0300

    fix err

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/nktau/monitoring-service/internal/server"
+import (
+	"github.com/nktau/monitoring-service/internal/applayer"
+	"github.com/nktau/monitoring-service/internal/httplayer"
+	"github.com/nktau/monitoring-service/internal/storagelayer"
+)
 
 func main() {
-	server.StartServer()
+	storeLayer := storagelayer.New()
+
+	// create app layer
+	appLayer := applayer.New(storeLayer)
+
+	// create http layer
+	httplayer.New(appLayer)
+
 }
