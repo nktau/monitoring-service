bf2f41e8c163c09b038ae833ade8844c56a7e8ed
nnaumov <lahbahx@gmail.com>
2023-07-14 17:12:58 +0300

    try to fix json

--- internal/server/httplayer/update_handlers.go
+++ internal/server/httplayer/update_handlers.go
@@ -187,5 +187,5 @@ func (api *httpAPI) updates(w http.ResponseWriter, r *http.Request) {
 	//}
 	//fmt.Printf("updates() my answer on request with body %s\n is %s\n", string(body), string(responseBody))
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	w.Write([]byte("{}"))
 }
