d2bdb6b2002f4ab076435eafb730db79cbfb677e
ni.v.naumov <ni.v.naumov@tinkoff.ru>
2024-04-15 10:42:48 +0300

    add headers to error log on the agent

--- internal/agent/app/agent.go
+++ internal/agent/app/agent.go
@@ -214,9 +214,16 @@ func (mem *agent) makeAndDoRequest(chMetrics chan []Metrics) error {
 		req.Header.Set("Content-Encoding", "gzip")
 		res, err := http.DefaultClient.Do(req)
 		if err != nil {
+			headers := ""
+			for header, _ := range req.Header {
+				headers += fmt.Sprintf("%s: %s, ", header, req.Header.Get(header))
+			}
 			mem.logger.Error("can't send metric to the server",
 				zap.Error(err),
-				zap.String("request body: ", string(requestBody)))
+				zap.String("request body: ", string(requestBody)),
+				zap.String("request headers", headers),
+			)
+
 			count := 0
 			for {
 				time.Sleep(time.Second)
@@ -224,9 +231,16 @@ func (mem *agent) makeAndDoRequest(chMetrics chan []Metrics) error {
 				if count == 1 || count == 4 || count == 9 {
 					res, err = http.DefaultClient.Do(req)
 					if err != nil {
+						headers = ""
+						for header, _ := range req.Header {
+							headers += fmt.Sprintf("%s: %s, ", header, req.Header.Get(header))
+						}
 						mem.logger.Error("can't send metric to the server",
 							zap.Error(err),
-							zap.String("request body: ", string(requestBody)))
+							zap.String("request body: ", string(requestBody)),
+							zap.String("request headers", headers),
+						)
+
 						if count == 9 {
 							break
 						}
